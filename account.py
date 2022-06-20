7211de4fb0b5fe0bdcb76270a6ecc0c2e1eef640
Liliane-Gikuku <lilkundiro@gmail.com>
2022-06-20 21:11:03 +0300

    Add currency

--- account.py
+++ account.py
@@ -8,6 +8,7 @@ class Account:
         self.id=id
         self.balance=0
         self.deposits=[]
+        self.new_account=0
 
 
         #Add a new attribute to the class Account called deposits which by default is an empty list.
@@ -25,20 +26,20 @@ class Account:
          if amount<=0:
              return f"Hello customer, you can't withdraw. Withdrawal should be greater than zero"
          elif amount>self.balance:
-             return f"Hello customer,your balance is {self.balance},You can't withdraw {amount}"
+             return f"Hello customer,your balance is {self.balance}Frw,You can't withdraw {amount}Frw."
          elif amount<self.balance+ self.transaction:
-            return f"Hello customer, your balance is {self.balance}. There is no transaction fee to withdraw {amount}"
+            return f"Hello customer, your balance is {self.balance}Frw. There is no transaction fee to withdraw {amount}Frw."
              
          else:
              self.balance-=amount
-             dct={"date":date.strftime("%d/%m/%Y"),"amount":amount,"narration":f'thank you for withdrawing {amount} on {date}'}
+             dct={"date":date.strftime("%d/%m/%Y"),"amount":amount,"narration":f'thank you for withdrawing {amount}Frw on {date}'}
              self.withdrawals.append(dct)
              withdrawal_amount=self.balance-self.transaction
          if amount>withdrawal_amount:
               return "Hello customer, your balance is insufficient to withdraw"
          self.balance-=amount+self.transaction
           #to include a transaction fee of 100 per transaction.
-         return f"Hello {self.name}, You have withdrawn {self.withdrawls} and your new balance is {self.balance} on {date.strftime('%d/%m/%Y')}). You paid 100 for the transaction."
+         return f"Hello {self.name}, You have withdrawn {self.withdrawls}Frw and your new balance is {self.balance}Frw on {date.strftime('%d/%m/%Y')}). You paid 100 for the transaction."
          
            
           
@@ -49,9 +50,9 @@ class Account:
             return f"Deposit must be greater than zero(0)"
         else:
          self.balance+=amount
-         dct={"date":date.strftime("%d/%m/%Y"),"amount":amount,"narration":f'thank you for depositing {amount} on {date}'}
+         dct={"date":date.strftime("%d/%m/%Y"),"amount":amount,"narration":f'thank you for depositing {amount}Frw on {date}'}
          self.deposits.append(dct)
-        return f"Hello {self.name}, You have deposited {amount}. Your new balance is {self.balance}"
+        return f"Hello {self.name}, You have deposited {amount}Frw. Your new balance is {self.balance}Frw."
     #Add a new method called deposits_statement which using a for loop prints each deposit in a new line
 
     def deposits_statement(self):
@@ -64,7 +65,7 @@ class Account:
 
  #Add a method to show the current balance.
     def current_balance(self):
-        return f"Your current balance is {self.balance}"
+        return f"Your current balance is {self.balance}Frw."
 
     def full_statement(self):
         statement=self.deposits+self.withdrawals
@@ -82,22 +83,22 @@ class Account:
         if amount>sum/3:
            return f"Hello customer, you can only borrow upto {sum/3}"
         if self.balance!=0:
-           return f"Hello customer, you have {self.balance} on your balance. You can't borrow yet you still have balance on your account."
+           return f"Hello customer, you have {self.balance}Frw on your balance. You can't borrow yet you still have balance on your account."
 
 
         if self.loan_balance!=0:
-            return f"Hello customer, you have a debt of {self.loan_balance} you have to pay first for you to borrow."
+            return f"Hello customer, you have a debt of {self.loan_balance}Frw you have to pay first for you to borrow."
         else:
             interest= 3/100*(amount)
             self.loan_balance+=amount+interest
-            return f"Hello customer, you have borrowed {amount}. Your loan is now at {self.loan_balance}"
+            return f"Hello customer, you have borrowed {amount}Frw. Your loan is now at {self.loan_balance}Frw."
     
     def loan_repayment(self,amount):
         
          if amount>self.loan_balance:
              self.balance+=amount-self.loan_balance
              self.loan_balance=0
-             return f" Hello customer, thank you for paying the loan of {amount-self.loan_balance} your account balance is {self.balance}"      
+             return f" Hello customer, thank you for paying the loan of {amount-self.loan_balance}Frw your account balance is {self.balance}Frw."      
          else:
              self.loan_balance-=amount
              return f"Thank you"
@@ -111,7 +112,7 @@ class Account:
         if isinstance(new_account,Account):
             self.balance-=amount
             new_account.balance+=amount
-            return f"Dear customer, you have sent {amount} to {new_account} with the name {new_account.name}.your new balance is {self.balance}.Thank you."
+            return f"Dear customer, you have sent {amount}Frw to {new_account} with the name {new_account.name}.your new balance is {self.balance}Frw.Thank you."
 
 
 
